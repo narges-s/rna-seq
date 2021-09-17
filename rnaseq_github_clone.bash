9cc6cc47cbbab526102cd0e067a578ae7a3c2ec7
narges-s <90798193+narges-s@users.noreply.github.com>
2021-09-17 13:51:15 +0300

    Update rnaseq_github_clone.bash

--- rnaseq_github_clone.bash
+++ rnaseq_github_clone.bash
@@ -6,7 +6,8 @@
 cd /home
 # git reflog requires a name and email if user is not in passwd
 # even if you're only cloning
-wget https://raw.githubusercontent.com/narges-s/rna-seq/main/cnt.lunglymph.tsv
+wget https://raw.githubusercontent.com/narges-s/rna-seq/main/counttab.csv
+wget https://raw.githubusercontent.com/narges-s/rna-seq/main/annottab.cvs
 export GIT_COMMITTER_NAME=narges-s
 export GIT_COMMITTER_EMAIL=soren.narges@gmail.com
 git clone https://github.com/narges-s/rna-seq.git
